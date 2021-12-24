45bc3160a6fcae8a4b21d1157dce62c4d0a57bf1
Dante Sivo <dvs8452@g.rit.edu>
2021-12-24 08:10:59 +0000

    Daylight strobe configuration. Looks pretty. Will need to integrate with brightness control

--- daylight_strobe.py
+++ daylight_strobe.py
@@ -2,6 +2,7 @@ import time
 from rpi_ws281x import *
 import RPi.GPIO as GPIO
 import math
+import random
 
 LED_COUNT       = 200
 LED_PIN         = 12
@@ -21,10 +22,33 @@ def clearStrip():
         strip.setPixelColor(i, Color(0,0,0))
     strip.show()
 
-def setStripRGB(R, G, B):
+def use_tolerance(C, tolerance):
+    output = C + tolerance
+    if (output > 255):
+        return 255
+    elif (output < 0):
+        return 0
+    elif (output < 40):
+        return C
+    else:
+        return output
+
+
+def setStripRGB(colorArray):
     for i in range(0, strip.numPixels()):
-        strip.setPixelColor(i , Color(R,G,B))
+        strip.setPixelColor(i, colorArray[i])
     strip.show()
+
+def shift_right(list, num):
+    output_list = []
+
+    for item in range(len(list) - num, len(list)):
+        output_list.append(list[item])
+
+    for item in range(0, len(list) - num):
+        output_list.append(list[item])
+
+    return output_list
 
 if __name__ == '__main__':
     # Create NeoPixel object with parameters from top of file
@@ -32,7 +56,7 @@ if __name__ == '__main__':
     # Initalize library
     strip.begin()
 
-    GPIO.setmode (GPIO.BOARD)
+    GPIO.setmode(GPIO.BOARD)
     GPIO.setup(RELAY_PIN, GPIO.OUT)
     GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
@@ -42,14 +66,43 @@ if __name__ == '__main__':
 
     GPIO.output(RELAY_PIN,GPIO.LOW) # Enable RaspPi control
 
+    R = 240
+    G = 240
+    B = 50
+
+    tL = -20
+    tH = 20
+
+    delay = 0.1
+
+    colors = [Color(0,0,0)] * LED_COUNT
+    pulse_width = 4
+
+    clearStrip()
+
     while True:
-        if (c < 255):
-            c = c + 1
-        else:
-            c = 255
-        
-        if (GPIO.input(BUTTON_PIN) == GPIO.HIGH):
-           c = 0
-        print(GPIO.input(BUTTON_PIN))
-        setStripRGB(c,c,math.floor(c/7))
-        time.sleep(0.01)
+        cR = R - random.randint(tL,tH)
+        cG = G - random.randint(tL,tH)
+        cB = B - random.randint(tL,tH)
+        if (cR > 255):
+            cR = 255
+        elif (cR < 0):
+            cR = abs(cR)
+
+        if (cG > 255):
+            cG = 255
+        elif (cG < 0):
+            cG = abs(cG)
+
+        if (cB > 255):
+            cB = 255
+        elif (cB < 0):
+            cB = abs(cB)
+
+        print (str(cR) + ":" + str(cG) + ":" + str(cB))
+        colors = shift_right(colors, pulse_width)
+        for i in range(0, pulse_width):
+            colors[i] = Color(cR, cG, cB)
+       
+        setStripRGB(colors)
+        time.sleep(delay)
